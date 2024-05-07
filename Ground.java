b01262b92989d890b7332c8d609a66f264614ae3
Vincent Qin <vqin621@student.fuhsd.org>
2024-05-07 13:27:25 -0700

    javadoc

--- Ground.java
+++ Ground.java
@@ -1,17 +1,33 @@
 import java.util.*;
 import TunnelMaker;
 
+/**
+ * @author Vincent Qin
+ * @version 1.0
+ */
 public class Ground extends Board{
     private int[][] grid;
 
+    /**
+     * instantiates a 2d array of numbers from 0-3
+     * 0 represents dirt, 1 represents air, 2 represents cannonballs, and 3 represents eggs
+     * @param r number of rows
+     * @param c number of columns
+     */
     public Ground(int r, int c) {
         super(r, c);
     }
 
+    /**
+     * returns the item at r, c
+     */
     public int getItem(int r, int c) {
         return grid[r][c];
     }
 
+    /**
+     * remakes the array using tunnelmaker to generate random tunnels, will add eggs later
+     */
     public void reset() {
         Arrays.fill(grid, 0);
         TunnelMaker t = new TunnelMaker();
@@ -20,10 +36,21 @@ public class Ground extends Board{
         }
     }
 
+    /**
+     * set item at r, c to int v (meanings in constructor)
+     * @param r row to set
+     * @param c col to set
+     * @param v the item to set it to
+     */
     public void setItem(int r, int c, int v) {
         grid[r][c] = v;
     }
 
+    /**
+     * counts a specfic item, will usually be for eggs, sometimes for cannonballs
+     * @param item the item to count
+     * @return returns the number of items in grid
+     */
     public int countItem(int item) {
         int ans = 0;
         for (int i = 0; i < grid.length; i++) {
