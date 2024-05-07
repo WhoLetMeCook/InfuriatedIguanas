33df7addb221f074ab0fc1188a2bbd4e61d0c2d2
Vincent Qin <vqin621@student.fuhsd.org>
2024-05-07 13:02:01 -0700

    goober

---
+++ Board.java
@@ -0,0 +1,23 @@
+public abstract class Board {
+    private int row;
+    private int col;
+    private int[][] board;
+
+    public Board(int r, int c) {
+        row = r;
+        col = c;
+        board = new int[row][col];
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getItem(int r, int c) {
+        return board[r][c];
+    }
+}
