0185593116ad029d00d51ae577107d935f508ca2
ichou21 <114119955+ichou21@users.noreply.github.com>
2024-05-09 20:29:49 +0000

    change display.java

---
+++ Display.java
@@ -0,0 +1,13 @@
+/**
+ * @author I Chen Chou
+ * @version 5/9/24
+ */
+import java.swing.*;
+import java.awt.*;
+
+public class Display {
+    private GridLayout grid;
+    public Display(int r, int c) {
+        grid = new GridLayout(r, c);
+    }
+}
