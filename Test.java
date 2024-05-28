42a7df353009e497f3295bb4c9be3402f747cb2a
ichou21 <114119955+ichou21@users.noreply.github.com>
2024-05-28 19:31:49 +0000

    display the iguana

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        Ground cur = new Ground(50, 100, 10, 10);
+        Ground cur = new Ground(100, 100, 20, 10);
         cur.reset();
         cur.displayGrid();
     }
